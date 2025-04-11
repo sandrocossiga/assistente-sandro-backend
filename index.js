5cee690aa81517a3d3cebd638c0f34a44ca6c275
sandrocossiga <sandrocossiga@gmail.com>
2025-04-12 00:59:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const { google } = require('googleapis');
 
 const auth = new google.auth.GoogleAuth({
   keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-  scopes: ['https://www.googleapis.com/auth/drive']
+  scopes: [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/gmail.send'
+  ]
 });
 
 const driveService = google.drive({ version: 'v3', auth });
