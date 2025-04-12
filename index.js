2bba3401b0701d6565c2249ed9959300ea308f0e
sandrocossiga <sandrocossiga@gmail.com>
2025-04-12 11:37:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,19 @@ const auth = new google.auth.GoogleAuth({
 });
 
 const driveService = google.drive({ version: 'v3', auth });
-const gmail = google.gmail({ version: 'v1', auth });
+
+const { google } = require('googleapis');
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GMAIL_CLIENT_ID,
+  process.env.GMAIL_CLIENT_SECRET,
+  process.env.GMAIL_REDIRECT_URI
+);
+
+oAuth2Client.setCredentials({ refresh_token: process.env.GMAIL_REFRESH_TOKEN });
+
+const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+
 
 const app = express();
 app.use(bodyParser.json());
